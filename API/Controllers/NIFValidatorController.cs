fcc161a9cc847bedc218b00b7c254827addf22e2
Luiz Camargo <luiz.zito@hotmail.com>
2022-06-10 19:44:32 -0300

    change endpoint route

--- API/Controllers/NIFValidatorController.cs
+++ API/Controllers/NIFValidatorController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Validate")]
     public class NIFValidatorController : ControllerBase
     {      
         [HttpGet]
